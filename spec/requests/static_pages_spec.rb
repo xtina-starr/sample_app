6af620421e99e511f6b6f68c67835236ea0717fc
Christina Thompson <c.thompsonms@gmail.com>
2013-12-07 15:29:45 -0800

    Created title test for home, help, and pages (RED, GREEN, REFACTOR

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
   end
 
   describe "Help page" do
@@ -14,12 +19,22 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Help')
+    end
   end
 
   describe "About page" do
     it "should have content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About Us')
     end
   end
 end
